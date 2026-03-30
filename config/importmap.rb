deb08c63dca72b7f047e7e204401e10dbc505054
Renato Santos <renatosan@>
2026-03-30 16:58:28 +0000

    Integrate preline with importmap and turbo auto-init

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,9 +4,7 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-# Bootstrap JS
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.8/dist/js/bootstrap.esm.js"
+pin "preline", to: "preline.js", preload: true
 
 pin_all_from "app/javascript/lib/", under: "lib"
 pin_all_from "app/javascript/controllers", under: "controllers"
