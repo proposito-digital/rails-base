e45afc6cfb92115579092ebe64eee9770094e45d
Renato Santos <renatosan@>
2026-04-02 19:53:21 +0000

    Adjust pagy locale

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,7 +3,8 @@ class Admin::BaseController < ApplicationController
   include Translations::TranslationFlashMessages
   include SidebarConcerns
   include Pundit::Authorization
-  #before_action { Pagy::I18n.locale = I18n.locale.to_s.tr("_", "-").sub(/-[a-z]{2}\z/) { |region| region.upcase } }
+
+  before_action :set_pagy_locale
   before_action :set_menu # SidebarConcerns
   before_action :set_model_class
   before_action :set_instance_and_authorize, only: %i[ show edit update destroy ]
@@ -11,6 +12,7 @@ class Admin::BaseController < ApplicationController
   def pundit_user
     true
   end
+
   # GET /admin instance/or /admin.instance/json
   def index
     @pagy, @instances = pagy(@model.all, limit: 10)
@@ -78,6 +80,11 @@ class Admin::BaseController < ApplicationController
     end
 
   private
+
+    def set_pagy_locale
+      Pagy::I18n.locale = I18n.locale.to_s.tr("_", "-").sub(/-[a-z]{2}\z/) { |region| region.upcase }
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_instance_and_authorize
       @instance = @model.find(params.expect(:id))
