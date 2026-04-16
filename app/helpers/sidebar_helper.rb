2010ccce6507ffcbafa427dc51023a7725c902b3
Renato Santos <98054462+renatosan-ti@users.noreply.github.com>
2026-04-16 11:48:08 -0300

    Update the front-end on mobile (mobile first) (#16)
    * Stabilize mobile drawer toggle and preserve desktop collapse behavior
    
    * Enable language selector in mobile header
    
    * Apply mobile-first layout improvements across admin screens
    
    * Update login and password flows for mobile-first experience
    
    * Use translated active menu label as mobile navbar title
    
    * Change edit form secondary action from View to Cancel and redirect to index
    
    ---------
    
    Co-authored-by: Renato Santos <renatosan@>

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 module SidebarHelper
-
-	def menu_active? current_menu, text = false
+  def menu_active?(current_menu, text = false)
     if controller_name.to_sym == current_menu[:url][:controller].to_sym
-      if text 
-        return 'active'
-      else
-        return true
-      end
+      return "active" if text
+
+      return true
     end
-	end
-	
+  end
+
+  def mobile_navbar_title
+    active_menu = Array(@menu).find { |menu_item| menu_item[:active] }
+    return active_menu[:name] if active_menu&.dig(:name).present?
+
+    translated_title = t("#{controller_name}.plural", default: "")
+    return translated_title if translated_title.present?
+
+    controller_name.humanize
+  end
 end
