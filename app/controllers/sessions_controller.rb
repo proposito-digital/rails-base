e158d2c5b87c8565b204b5a1aef85ef2d0d88663
Diogo Caetano <diogo.caetano.alves@gmail.com>
2026-08-14 18:54:54 -0300

    Revisar os fluxos de criação, edição e remoção de usuários.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,18 @@
 class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
-  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: t("authentication.sessions.rate_limited") }
 
   def new
   end
 
   def create
-    if user = User.authenticate_by(params.permit(:email_address, :password))
+    user = User.authenticate_by(params.permit(:email_address, :password))
+
+    if user&.active?
       start_new_session_for user
       redirect_to after_authentication_url
     else
-      redirect_to new_session_path, alert: "Try another email address or password."
+      redirect_to new_session_path, alert: t("authentication.sessions.invalid_credentials")
     end
   end
 
